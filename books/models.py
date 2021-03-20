b46c05b35034abed72c53d8668cf4e1dbb6d5c75
Tomasz Rzęsikowski <tomasz.rzesikowski@gmail.com>
2021-03-20 17:38:06 +0100

    feat(books): create models for Author, PublicationLanguage, Book

---
+++ books/models.py
@@ -0,0 +1,39 @@
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+def on_language_delete():
+    return PublicationLanguage.objects.get_or_create(language="unknown")[0]
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, null=False, default=None, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class PublicationLanguage(models.Model):
+    language = models.CharField(max_length=50, unique=True, null=False, default=None)
+
+    def __str__(self):
+        return f"{self.language}"
+
+
+class Book(models.Model):
+    isbn = models.CharField(validators=[
+        RegexValidator(regex=r"(\d{12}[0-9X]{1})", message="ISBN has to be 13 character long.")
+    ], max_length=13, unique=True, verbose_name="ISBN")
+
+    title = models.CharField(max_length=255, null=False)
+    author = models.ManyToManyField("Author")
+    publication_year = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(9999)], null=False)
+    page_count = models.PositiveIntegerField(null=False)
+    cover = models.URLField()
+
+    publication_language = models.ForeignKey(
+        "PublicationLanguage",
+        on_delete=models.SET(on_language_delete))
+
+    def __str__(self):
+        return f"Book \"{self.title}\". ISBN: {self.isbn}"
--- books_poc/settings.py
+++ books_poc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", '').split(" ")
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'books.apps.BooksConfig'
 ]
 
 MIDDLEWARE = [
