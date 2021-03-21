a19569149dd44d3f1046a4f287e1f3afa9221d05
Tomasz Rzęsikowski <tomasz.rzesikowski@gmail.com>
2021-03-21 13:49:13 +0100

    feat(books): create update book web view

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,7 @@ app_name = 'books'
 urlpatterns = [
     path('', views.ListBookView.as_view(), name='list-books'),
     path('create', views.CreateBookView.as_view(), name='create-book'),
+    path('<int:pk>', views.UpdateBookView.as_view(), name='update-book'),
     path('create_author', views.CreateAuthorView.as_view(), name='create-author'),
     path('create_pub_lang', views.CreateLanguageView.as_view(), name='create-pub-lang')
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from books.models import Book, Author, PublicationLanguage
 
@@ -38,10 +38,18 @@ class CreateBookView(CreateView):
     success_url = reverse_lazy('books:list-books')
 
 
+class UpdateBookView(UpdateView):
+    model = Book
+    template_name = "books/update_book.html"
+    fields = ("isbn", "title", "author", "publication_year", "page_count", "cover", "publication_language")
+    raise_exception = True
+    success_url = reverse_lazy('books:list-books')
+
+
 class CreateAuthorView(CreateView):
     model = Author
     template_name = "books/create_author.html"
-    fields = ("name", )
+    fields = ("name",)
     raise_exception = True
     success_url = reverse_lazy('books:create-book')
 
@@ -49,6 +57,6 @@ class CreateAuthorView(CreateView):
 class CreateLanguageView(CreateView):
     model = PublicationLanguage
     template_name = "books/create_pub_lang.html"
-    fields = ("language", )
+    fields = ("language",)
     raise_exception = True
     success_url = reverse_lazy('books:create-book')
