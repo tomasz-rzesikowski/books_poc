866bd39ac4294aa736b46b61aa96a388da9f6595
Tomasz Rzęsikowski <tomasz.rzesikowski@gmail.com>
2021-03-23 10:21:39 +0100

    fix(app): configure django-toolbar

--- books_poc/urls.py
+++ books_poc/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from books_poc import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include("books.urls")),
     path('v1/', include("api.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
