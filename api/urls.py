b413a8d78c5ac8c63fee60a8f0b4c1b9b677167c
Tomasz Rzęsikowski <tomasz.rzesikowski@gmail.com>
2021-03-23 19:51:00 +0100

    test(api): add views tests for api

--- api/urls.py
+++ api/urls.py
@@ -16,6 +16,8 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+app_name = 'api'
+
 urlpatterns = [
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('books/', BookList.as_view(), name="book_list")
